dd2566a8cf157b999c79fb0c56fe00d9e9a13944
cel7t <shahsarthakw@gmail.com>
2023-02-20 01:17:52 +0530

    Font Update

--- config.def.h
+++ config.def.h
@@ -6,9 +6,9 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"TamzenForPowerline:size=16", "Noto Color Emoji:pixelsize=22"};
+static const char *fonts[]          = {"Iosevka:size=16", "Noto Color Emoji:pixelsize=22"};
 //{ "M+ 1m:size=14", "Noto Color Emoji:pixelsize=22" };
-static const char dmenufont[]       = "TamzenForPowerline:size=16";
+static const char dmenufont[]       = "Iosevka:size=16";
 static const char menuconf[]		= "$HOME/.9menu";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
